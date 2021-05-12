9230c8270cce36a83a4d9427bd54d1741abfde37
Nick Rizzo <nickrizzo77277@gmail.com>
2021-05-11 19:13:10 -0600

    Added table of contents tags

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -30,6 +30,11 @@ function generateMarkdown(data) {
   ## Table of Contents
   - [Description](#Description)
   - [Installation](#Installation)
+  - [Usage] (#Usage)
+  - [License](#License)
+  - [Contributing](#Contributing)
+  - [Tests](#Tests)
+  - [Questions](#Questions)
 
   ### Installation 
   ${data.install}
