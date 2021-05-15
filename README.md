e5ea862999b091826f5ea7181f1ce1a6705fcfac
Nick Rizzo <nickrizzo77277@gmail.com>
2021-05-15 14:55:04 -0600

    fixed file structure

--- README.md
+++ README.md
@@ -1,3 +1,39 @@
-# Professional README Generator Starter Code
+# README Generator
 
-[How to create a Professional README](./readme-guide.md)
+  ## Description 
+  This application is designed to make the process of creating a GitHub readme a little easier.
+
+  ## Table of Contents
+  - [Description](#Description)
+  - [Installation](#Installation)
+  - [Usage](#Usage)
+  - [License](#License)
+  - [Contribute](#Contribute)
+  - [Testing](#Testing)
+  - [Questions](#Questions)
+
+  ## Installation 
+  npm install
+
+  ## Usage
+  clone, run node index.js
+
+  ## License
+   ![GitHub License](https://img.shields.io/badge/MIT-Version-blue) 
+
+  - [Click here](https://opensource.org/licenses/MIT) for more information on the license.
+
+  ## Contribute
+  none
+
+  ## Testing
+  none
+
+  ## Questions
+
+Feel free to reach out with any questions
+
+- [nickrizzo77277@gmail.com](mail.to:nickrizzo77277@gmail.com) <br>
+- [GitHub](https://github.com/izzo2323)
+
+  
---
+++ index.js
@@ -0,0 +1,128 @@
+// TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const markdown = require('./utils/generateMarkdown');
+// TODO: Create an array of questions for user input
+const questions =
+    [
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Please enter your name. (Required)',
+            validate: (nameInput) => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your name!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'githubname',
+            message: 'Please enter your GitHub username? (Required)',
+            validate: (githubname) => {
+                if (githubname) {
+                    return true;
+                } else {
+                    console.log('Please enter your GitHub username!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please enter your email. (Required)',
+            validate: (email) => {
+                if (email) {
+                    return true;
+                } else {
+                    console.log('Please enter your email!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'projectname',
+            message: 'Please enter the name of your application. (Required)',
+            validate: (projectNameInput) => {
+                if (projectNameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a Project Name!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please enter a description of your application. (Required)',
+            validate: descriptioninput => {
+                if (descriptioninput) {
+                    return true;
+                } else {
+                    console.log('Please enter a description!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Please enter what licence this project has.',
+            choices: ['MIT', 'GPLv2', 'GPLv3', 'Apache', 'other', 'none'],
+        },
+        {
+            type: 'input',
+            name: 'install',
+            message: 'What infomation is needed to install and run this application?',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'What information is needed about using this application?'
+        },
+        {
+            type: 'input',
+            name: 'contribute',
+            message: 'What information is needed about contributing to the project?'
+        },
+        {
+            type: 'input',
+            name: 'testing',
+            message: 'What information should be included about testing this application?'
+        },
+    ];
+
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    console.log("writetofile data", data)
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            console.log('This is wrong');
+        } else {
+            console.log('README created successfully');
+        }
+    })
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions)
+        .then(answer => {
+            console.log(answer);
+            writeToFile('README.md', markdown(answer));
+
+        })
+}
+
+// Function call to initialize app
+init();
+
+// let answers = questions();
+// console.log(answers)
